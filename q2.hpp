e4653d3e8aa77c25fcba47cfda767d10681d2c5c
Mohsen <mmakhlaqi@gmail.com>
2023-03-21 15:11:57 -0700

    hello

--- q2.hpp
+++ q2.hpp
@@ -18,13 +18,6 @@ void printResult(int n1, int n2, int n3, int min) {
     cout << "The minimum of " << n1 << ", " << n2 << ", " << n3 << " is " << min << endl;
 }
 
-int main() {
-    int n1, n2, n3, min;
-    getInput(n1, n2, n3);
-    min = findMin(n1, n2, n3);
-    printResult(n1, n2, n3, min);
-    return 0;
-}
 // ******************************
 // Implement all your functions here
 // ******************************
