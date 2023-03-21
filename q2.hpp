c274138aeb4bab9418838f67b0b2e78fcc78f9aa
Mohsen <mmakhlaqi@gmail.com>
2023-03-21 15:09:33 -0700

    hello

--- q2.hpp
+++ q2.hpp
@@ -2,6 +2,29 @@
 #include <iomanip>
 using namespace std;
 
+void getInput(int &n1, int &n2, int &n3) {
+    cout << "Enter three numbers: ";
+    cin >> n1 >> n2 >> n3;
+}
+
+int findMin(int n1, int n2, int n3) {
+    int min = n1;
+    if (n2 < min) min = n2;
+    if (n3 < min) min = n3;
+    return min;
+}
+
+void printResult(int n1, int n2, int n3, int min) {
+    cout << "The minimum of " << n1 << ", " << n2 << ", " << n3 << " is " << min << endl;
+}
+
+int main() {
+    int n1, n2, n3, min;
+    getInput(n1, n2, n3);
+    min = findMin(n1, n2, n3);
+    printResult(n1, n2, n3, min);
+    return 0;
+}
 // ******************************
 // Implement all your functions here
 // ******************************
